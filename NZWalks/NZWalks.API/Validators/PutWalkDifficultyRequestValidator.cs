dd020e16df05b22e948a475cab846f370db57fb8
Felipe Ribeiro <felipeonlinebr@gmail.com>
2023-02-24 21:01:47 -0300

    Fim da seção 07

---
+++ NZWalks/NZWalks.API/Validators/PutWalkDifficultyRequestValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace NZWalks.API.Validators
+{
+    public class PutWalkDifficultyRequestValidator : AbstractValidator<Models.DTO.PutWalkDifficultyRequest>
+    {
+        public PutWalkDifficultyRequestValidator()
+        {
+            RuleFor(x => x.Code).NotEmpty();
+        }
+    }
+}
---
+++ NZWalks/NZWalks.API/Validators/PutWalkRequestValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace NZWalks.API.Validators
+{
+    public class PutWalkRequestValidator : AbstractValidator<Models.DTO.PutWalkRequest>
+    {
+        public PutWalkRequestValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty();
+            RuleFor(x => x.Length).GreaterThan(0);
+        }
+    }
+}
